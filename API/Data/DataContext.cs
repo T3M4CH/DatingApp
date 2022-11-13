6737c9290e0279fd7e5d3d5663aef2f3394e7f7a
Artyom <picheneg.epta@gmail.com>
2022-11-13 08:31:51 +0300

    DatingApp
    Initialize my first ASP.Net program

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public sealed class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        
+        //optionsBuilder.UseSqlite()
+    }
+
+    public DbSet<AppUser?> Users { get; set; }
+}
