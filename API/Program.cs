661ad52eb25c8d5c5f5fa49b01cdd3283f1278c0
Artyom <picheneg.epta@gmail.com>
2022-11-22 20:33:26 +0300

    https

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
